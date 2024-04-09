6e137febcec780b84a79bf188496884fee74afd6
Nick Ito <nick.yusuke.ito@gmail.com>
2024-04-09 15:08:11 -0700

    refactor: transition from polyrepo to monorepo

---
+++ express-server/src/lambda.ts
@@ -0,0 +1,10 @@
+// // Example: src/lambda.ts
+// import * as awsServerlessExpress from 'aws-serverless-express';
+
+// import { app } from '@/app';
+
+// const server = awsServerlessExpress.createServer(appÍ);
+
+// exports.handler = (event, context) => {
+//   awsServerlessExpress.proxy(server, event, context);
+// };
