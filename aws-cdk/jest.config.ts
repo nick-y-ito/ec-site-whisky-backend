2351e321d70f5b2df41dba907c35b0a53f35c1bf
Nick Ito <nick.yusuke.ito@gmail.com>
2024-04-09 15:38:18 -0700

    chore: cdk init app --language=typescript

---
+++ aws-cdk/jest.config.ts
@@ -0,0 +1,9 @@
+/** @type {import("ts-jest/dist/types").InitialOptionsTsJest} */
+module.exports = {
+  testEnvironment: 'node',
+  roots: ['<rootDir>/test'],
+  testMatch: ['**/*.test.ts'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+};
